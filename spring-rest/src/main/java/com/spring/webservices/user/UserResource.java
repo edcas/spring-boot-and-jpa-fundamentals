ea39bae8b2d855c1c433984912eeafd43473e875
Eduard Castro <eduard.cas07@gmail.com>
2020-04-11 23:48:15 -0500

    Add user resource

---
+++ spring-rest/src/main/java/com/spring/webservices/user/UserResource.java
@@ -0,0 +1,25 @@
+package com.spring.webservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+	@Autowired
+	private UserDaoService userService;
+	
+	@GetMapping("/users")
+	public List<User> all() {
+		return this.userService.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public User show(@PathVariable int id) {
+		return this.userService.findOne(id);
+	}
+}
