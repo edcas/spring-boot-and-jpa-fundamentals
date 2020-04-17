55716a77f9f94f8a088aa34db853e66cec4edc2c
Eduard Castro <eduard.cas07@gmail.com>
2020-04-16 20:58:18 -0500

    Add HTTP status code 201 and Location - Create user resource

--- spring-rest/src/main/java/com/spring/webservices/user/UserResource.java
+++ spring-rest/src/main/java/com/spring/webservices/user/UserResource.java
@@ -1,32 +1,40 @@
 package com.spring.webservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
 
-	@Autowired
-	private UserDaoService userService;
+    @Autowired
+    private UserDaoService userService;
+
+    @GetMapping("/users")
+    public List<User> all() {
+	return this.userService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User show(@PathVariable int id) {
+	return this.userService.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+	User savedUser = this.userService.save(user);
+
+	URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+		.toUri();
 	
-	@GetMapping("/users")
-	public List<User> all() {
-		return this.userService.findAll();
-	}
-	
-	@GetMapping("/users/{id}")
-	public User show(@PathVariable int id) {
-		return this.userService.findOne(id);
-	}
-	
-	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		User savedUser = this.userService.save(user);
-	}
+	return ResponseEntity.created(location).build();
+    }
 }
