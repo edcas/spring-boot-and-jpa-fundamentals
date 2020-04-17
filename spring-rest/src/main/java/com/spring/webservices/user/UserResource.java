3a4a07dca6d8d2493bc379ef98603ca63811abc6
Eduard Castro <eduard.cas07@gmail.com>
2020-04-16 20:47:30 -0500

    Add create user resource

--- spring-rest/src/main/java/com/spring/webservices/user/UserResource.java
+++ spring-rest/src/main/java/com/spring/webservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +23,10 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User show(@PathVariable int id) {
 		return this.userService.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User savedUser = this.userService.save(user);
 	}
 }
