8b7eb40dcd5d13a0e9de46bfc143e0966eac681c
Eduard Castro <eecastro@sura.com.co>
2020-04-20 21:07:12 -0500

    Add DELETE method to delete a user resource

--- spring-rest/src/main/java/com/spring/webservices/user/UserResource.java
+++ spring-rest/src/main/java/com/spring/webservices/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,32 +16,43 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RestController
 public class UserResource {
 
-    @Autowired
-    private UserDaoService userService;
+	@Autowired
+	private UserDaoService userService;
 
-    @GetMapping("/users")
-    public List<User> all() {
-	return this.userService.findAll();
-    }
-
-    @GetMapping("/users/{id}")
-    public User show(@PathVariable int id) {
-	User user = this.userService.findOne(id);
-	
-	if(user == null) {
-	    throw new UserNotFoundException("id - " + id);
+	@GetMapping("/users")
+	public List<User> all() {
+		return this.userService.findAll();
 	}
-	
-	return user;
-    }
 
-    @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user) {
-	User savedUser = this.userService.save(user);
+	@GetMapping("/users/{id}")
+	public User show(@PathVariable int id) {
+		User user = this.userService.findOne(id);
 
-	URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
-		.toUri();
-	
-	return ResponseEntity.created(location).build();
-    }
+		if (user == null) {
+			throw new UserNotFoundException("id - " + id);
+		}
+
+		return user;
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = this.userService.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+
+	@DeleteMapping("/users/{id}")
+	public void delete(@PathVariable int id) {
+
+		User user = this.userService.deleteById(id);
+
+		if (user == null) {
+			throw new UserNotFoundException("id- " + id);
+		}
+
+	}
 }
